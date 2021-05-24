331329d8e8c4e2d797ac290d33e0b8f3367c8142
Moroine <moroine.bentefrit@gmail.com>
2021-05-24 16:17:31 +0200

    feat: list commits

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-// exit early
-if (!['pull_request_target', 'pull_request'].includes(github.context.eventName)) {
-  core.setFailed('action triggered outside of a pull_request')
-  process.exit(1)
+async function run () {
+  // exit early
+  if (!['pull_request_target', 'pull_request'].includes(github.context.eventName)) {
+    core.setFailed('action triggered outside of a pull_request')
+    process.exit(1)
+  }
+  
+  try {
+    const { repo, payload: { number } } = github.context;
+    const commits = await octokit.rest.pulls.listCommits({
+      ...repo,
+      pull_number: number,
+    });
+    
+    console.log(commits)
+    core.info(`Yah`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
-try {
-  const { repo, payload: { number } } = github.context;
-  console.log(repo, number)
-  core.info(`Yah`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+run();
