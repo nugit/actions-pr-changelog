60dccc4a67f8c8e855228aa2fd8d7f8ed7329425
Moroine <moroine.bentefrit@gmail.com>
2021-05-24 16:28:54 +0200

    feat: log commits individually

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function run () {
       pull_number: number,
     });
     
-    console.log(commits)
+    console.log(...commits.data[0])
     core.info(`Yah`);
   } catch (error) {
     core.setFailed(error.message);
