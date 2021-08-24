787da8fac25a7681cb0f1b3c6d2ccfb4864c67c2
Moroine <moroine.bentefrit@gmail.com>
2021-08-24 10:10:36 +0200

    chore: address PR comments

--- test/getPullRequest.js
+++ test/getPullRequest.js
@@ -1,5 +1,5 @@
 const { octokit } = require("./octokit");
-const { findPullRequest, getPullRequestCommits } = require("../lib/getPullRequest");
+const { findPullRequest } = require("../lib/getPullRequest");
 
 async function test() {
   const pr =  await findPullRequest(octokit, "nugit/nugit-visualization", "0559c0fa0");
