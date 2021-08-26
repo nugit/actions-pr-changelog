bf5e2410b975a77f03ad927833603387cb32bbd1
Moroine <moroine.bentefrit@gmail.com>
2021-08-26 08:56:48 +0200

    fix: repo name & owner variables

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ const github = require('@actions/github');
 const { updateReleasePR } = require('./lib/pr');
 
 async function run () {
-  console.log(github.context);
-
   // exit early
   if (!['pull_request_target', 'pull_request'].includes(github.context.eventName)) {
     core.setFailed('action triggered outside of a pull_request')
     process.exit(1)
   }
+  core.debug(`github.context: ${JSON.stringify(github.context, null, 2)}`);
   
   try {
-    const { repo, payload: { number } } = github.context;
+    const { payload: { number, repository: { name, owner: { login }} } } = github.context;
     const token = core.getInput('token');
     const octokit = github.getOctokit(token);
 
     const type = core.getInput('type', { required: true });
     switch(type) {
       case 'release':
-        await updateReleasePR(octokit, owner, repo, number);
+        await updateReleasePR(octokit, login, name, number);
         break;
       case 'onprem':
         core.setFailed('Not supported yet');
